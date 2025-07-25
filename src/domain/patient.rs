7138453560ef969ba55f571f83cd8a040f7c7765
darren277 <darren277@yahoo.com>
2025-07-25 15:21:25 +0000

    Domain patient.

---
+++ src/domain/patient.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Clone)]
+pub struct DomainPatient {
+    pub demographic_no: String,
+    pub first_name:    Option<String>,
+    pub last_name:     Option<String>,
+    pub date_of_birth: Option<String>, // ISO "YYYY-MM-DD"
+    pub location:      Option<(String, String, String, String)>, // city, province, country, postal
+    pub sex:           Option<String>, // "male" | "female" | "other" | ...
+    pub phone:         Option<String>,
+    pub email:         Option<String>,
+}
