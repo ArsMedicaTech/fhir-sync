b8a0f80f1c4b5a9df08b504991dfa5cc5768a890
darren277 <darren277@yahoo.com>
2025-07-25 15:21:46 +0000

    Main entry point.

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use tokio::{select, signal, sync::mpsc};
+use tracing::{info, error};
+
+mod binlog;
+mod webhook;
+mod api;
+
+pub mod domain;
+pub mod adapters;
+pub mod ext;
+
+pub mod proto {
+    pub mod fhir_sync;
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    // Shared channel: listeners push events; API consumes & forwards
+    let (tx, rx) = mpsc::channel::<Event>(1024);
+
+    let binlog_task   = tokio::spawn(binlog::run_binlog_listener(tx.clone()));
+    let webhook_task  = tokio::spawn(webhook::run_webhook_server(tx.clone()));
+    let api_task      = tokio::spawn(api::run_grpc_server(rx));
+
+    // graceful shutdown on Ctrl-C
+    select! {
+        res = binlog_task   => handle_exit("binlog",   res),
+        res = webhook_task  => handle_exit("webhook",  res),
+        res = api_task      => handle_exit("api",      res),
+        _  = signal::ctrl_c() => info!("Ctrl-C received, shutting down"),
+    };
+
+    Ok(())
+}
+
+fn handle_exit(name: &str, res: Result<anyhow::Result<()>, tokio::task::JoinError>) {
+    match res {
+        Ok(Ok(())) => info!("{name} task exited cleanly"),
+        Ok(Err(e)) => error!("{name} task exited with error: {e:?}"),
+        Err(e)     => error!("{name} task exited with join error: {e:?}"),
+    }
+}
+
+/// Central event enum; extend ad-hoc
+#[derive(Debug)]
+pub enum Event {
+    PatientUpsert(Patient),
+    //EncounterUpdate(Encounter),
+    // ...
+}
