7b07bbb5202a43bf6bfb775a9ae72d03b4fbd77d
darren277 <darren277@yahoo.com>
2025-07-27 11:36:37 +0000

    Fixed webhook server.

--- src/webhook.rs
+++ src/webhook.rs
@@ -4,11 +4,12 @@ use crate::domain::patient::DomainPatient;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::extract::Extension;
+use std::net::SocketAddr;
 
 #[axum::debug_handler]
 pub async fn handle_upsert(
-    Json(dto): Json<DomainPatient>,
     Extension(_tx): Extension<tokio::sync::mpsc::Sender<Event>>,
+    Json(dto): Json<DomainPatient>,
 ) -> impl IntoResponse + Send {
     println!("Got patient: {:?}", dto);
     // You can use tx.send(...) here if needed
@@ -19,9 +20,10 @@ pub async fn run_webhook_server(tx: tokio::sync::mpsc::Sender<Event>) -> Result<
     let app = Router::new()
         .route("/patient", post(handle_upsert))
         .layer(Extension(tx));
-    
-    let listener = tokio::net::TcpListener::bind(&"0.0.0.0:8080".parse()?)
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080".parse::<SocketAddr>()?)
         .await.unwrap();
+
     axum::serve(listener, app).await.unwrap();
     Ok(())
 }
