3f45901051aebc79f3111807f8a1b9ee69abd59d
darren277 <darren277@yahoo.com>
2025-07-27 11:59:45 +0000

    Wrapped proto debug checks in an optional debug block.

--- build.rs
+++ build.rs
@@ -6,29 +6,34 @@ use std::path::PathBuf;
 use walkdir::WalkDir;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=proto/fhir_sync.proto");
+    let debug = env::var("DEBUG").unwrap_or_else(|_| "false".to_string()) == "true";
 
-    if !fs::metadata("proto/fhir_sync.proto").is_ok() {
-        panic!("File not found: proto/fhir_sync.proto");
-    } else {
-        println!("Found proto file, proceeding with build.");
+    if debug {
+        // Perform debug-specific checks
+        println!("cargo:rerun-if-changed=proto/fhir_sync.proto");
+
+        if !fs::metadata("proto/fhir_sync.proto").is_ok() {
+            panic!("File not found: proto/fhir_sync.proto");
+        } else {
+            println!("Found proto file, proceeding with build.");
+        }
+
+        let out = Command::new(std::env::var("PROTOC").unwrap_or_else(|_| "protoc".into()))
+            .arg("--proto_path=proto")
+            .arg("--proto_path=.")
+            .arg("--descriptor_set_out=descriptor.pb")
+            .arg("proto/fhir_sync.proto")
+            .output()?;  // This gives us access to both stdout and stderr
+
+        if !out.status.success() {
+            eprintln!("protoc failed with status: {}", out.status);
+            eprintln!("stdout:\n{}", String::from_utf8_lossy(&out.stdout));
+            eprintln!("stderr:\n{}", String::from_utf8_lossy(&out.stderr));
+            panic!("protoc invocation failed");
+        }
+
+        println!("protoc succeeded, proceeding to compile with tonic_build");
     }
-
-    let out = Command::new(std::env::var("PROTOC").unwrap_or_else(|_| "protoc".into()))
-        .arg("--proto_path=proto")
-        .arg("--proto_path=.")
-        .arg("--descriptor_set_out=descriptor.pb")
-        .arg("proto/fhir_sync.proto")
-        .output()?;  // This gives us access to both stdout and stderr
-
-    if !out.status.success() {
-        eprintln!("protoc failed with status: {}", out.status);
-        eprintln!("stdout:\n{}", String::from_utf8_lossy(&out.stdout));
-        eprintln!("stderr:\n{}", String::from_utf8_lossy(&out.stderr));
-        panic!("protoc invocation failed");
-    }
-
-    println!("protoc succeeded, proceeding to compile with tonic_build");
 
     // Force set protoc path
     let protoc_path = "/usr/bin/protoc"; // or `which protoc` output
