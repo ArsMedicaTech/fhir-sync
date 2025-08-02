9a21918bb6cb4cc91bbd3f61cc31e879e7a914a5
darren277 <darren277@yahoo.com>
2025-08-02 09:41:45 -0700

    Made client gen script extensible.

--- gen-proto.ps1
+++ gen-proto.ps1
@@ -1,3 +1,9 @@
+param(
+  [Parameter(Mandatory=$true)]
+  [ValidateSet("python", "dart", "go", "js", "ts", "cpp")]
+  [string]$lang
+)
+
 $base = (Get-Location).Path -replace '\\', '/'
 
 # Include all *.proto except those in proto/google/protobuf/*
@@ -9,12 +15,41 @@ $protoFiles = Get-ChildItem -Recurse -Filter *.proto -Path ./proto |
     $_.FullName.Replace('\', '/').Replace($base, '/work')
   }
 
-if (-not (Test-Path "gen/client")) {
-  New-Item -ItemType Directory -Path "gen/client" | Out-Null
+
+# Create output dir if needed
+$outDir = "gen/client"
+if (-not (Test-Path $outDir)) {
+  New-Item -ItemType Directory -Path $outDir | Out-Null
+}
+
+# Set language-specific output option
+switch ($lang) {
+  "python" {
+    $outFlag = "--python_out=/work/gen/client"
+  }
+  "dart" {
+    $outFlag = "--dart_out=grpc:/work/gen/client"
+  }
+  "go" {
+    $outFlag = "--go_out=/work/gen/client --go-grpc_out=/work/gen/client"
+  }
+  "js" {
+    $outFlag = "--js_out=import_style=commonjs:/work/gen/client"
+  }
+  "ts" {
+    $outFlag = "--plugin=protoc-gen-ts=/usr/bin/protoc-gen-ts --ts_out=/work/gen/client"
+  }
+  "cpp" {
+    $outFlag = "--cpp_out=/work/gen/client"
+  }
+  default {
+    Write-Error "Unsupported language: $lang"
+    exit 1
+  }
 }
 
 docker run --rm -v "${PWD}:/work" -w /work rvolosatovs/protoc `
   --proto_path=/work/proto `
   --proto_path=/usr/include `
-  --python_out=/work/gen/client `
+  $outFlag `
   $protoFiles
